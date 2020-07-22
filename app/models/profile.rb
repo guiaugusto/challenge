d5b443bf3b403bb32609cb12b86ad85ebf754cd2
Guilherme Augusto <guilherme.francais@gmail.com>
2020-07-22 11:27:08 -0300

    Implements filters basic logic

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,20 @@
 class Profile < ApplicationRecord
+  include PgSearch::Model
+
+  pg_search_scope :search_by_term, against: [:profile_name, :github_username],
+                    using: {
+                      tsearch: {
+                        any_word: true,
+                        prefix: true
+                      }
+                    }
+
+  def self.search(query)
+    if query.present? and !query[:search].empty?
+      @profiles = Profile.search_by_term(query[:search])
+    else
+      @profiles = Profile.all
+    end
+  end
+
 end
