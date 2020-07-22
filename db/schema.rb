16c86a8846beb8b7b602ad7d80d367c1ebe96f68
Guilherme Augusto <guilherme.francais@gmail.com>
2020-07-22 10:49:36 -0300

    Configures postgres database and bootstrap framework

---
+++ db/schema.rb
@@ -0,0 +1,28 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_07_18_182117) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "profile_name"
+    t.string "github_address"
+    t.string "github_username"
+    t.integer "amount_of_followers"
+    t.integer "amount_of_following"
+    t.integer "amount_stars"
+    t.integer "amount_contributions"
+    t.string "github_profile_image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
