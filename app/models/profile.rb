16fd811f0c8e28f8e5373b1806fdcfaba4e85af9
Guilherme Augusto <guilherme.francais@gmail.com>
2020-07-23 22:32:14 -0300

    Implements validation to profile fields

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,9 @@
 class Profile < ApplicationRecord
   include PgSearch::Model
 
+  validates :profile_name, :github_profile_image, presence: true
+  validates :github_username, :github_address, uniqueness: true
+
   pg_search_scope :search_by_term, against: [:profile_name, :github_username],
                     using: {
                       tsearch: {
@@ -13,7 +16,7 @@ class Profile < ApplicationRecord
     if query.present? and !query[:search].empty?
       @profiles = Profile.search_by_term(query[:search])
     else
-      @profiles = Profile.all
+      @profiles = Profile.none
     end
   end
 
