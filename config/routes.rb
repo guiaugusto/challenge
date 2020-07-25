efa0099e34640dcf57c1e61287b27bab022e9fbc
Guilherme Augusto <guilherme.francais@gmail.com>
2020-07-25 09:43:23 -0300

    Creates router to rescan profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :profiles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  patch "profiles/:id/scan", to: "profiles#scan"
   root "application#home"
 end
