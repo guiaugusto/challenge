a3a88d8944c91bbaa90e940d36dd4889b72388cc
Guilherme Augusto <guilherme.francais@gmail.com>
2020-07-27 14:37:04 -0300

    Implements validation to github url and profile image url

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,20 @@
 class Profile < ApplicationRecord
   include PgSearch::Model
 
-  validates :profile_name, :github_profile_image, presence: true
-  validates :github_username, :github_address, uniqueness: true
+  validates :profile_name, presence: true
+  validates :github_username, uniqueness: true
+
+  validates :github_profile_image,
+            format: {
+              with: %r(avatars[0-9]+.githubusercontent.com\/u\/\w+)
+            },
+            uniqueness: true
+
+  validates :github_address,
+            format: {
+              with: %r(github\.com\/[\w]+[-\w]*)
+            },
+            uniqueness: true
 
   pg_search_scope :search_by_term, against: [:profile_name, :github_username, :organization, :location],
                     using: {
